c589e89b4b8b84e27e46dcdbce7bb8829992f4f0
alex <alexocegueda59@gmail.com>
2026-01-21 20:59:58 -0600

    changed courses to course

--- frontend/src/routes/course/[id]/+page.ts
+++ frontend/src/routes/course/[id]/+page.ts
@@ -1,17 +1,18 @@
 // src/routes/course/[id]/+page.ts
+import { error } from '@sveltejs/kit';
+
 export const load = async ({ fetch, params }) => {
-    
-    const response = await fetch('https://canvasocegueda.pythonanywhere.com/api/courses', {
-            credentials: 'include' 
-        });
-    
-    if (!response.ok) {
-        throw new Error("Course not found in database");
+    const res = await fetch(`https://canvasocegueda.pythonanywhere.com/api/course/${params.id}`, {
+        credentials: 'include' 
+    });
+
+    if (res.ok) {
+        const course = await res.json();
+        return { course };
     }
 
-    const course = await response.json();
-    
-    return {
-        course
+    // If 404 or 401, return a safe empty object so the page doesn't crash
+    return { 
+        course: { title: 'Course Not Found', modules: [] } 
     };
 };
